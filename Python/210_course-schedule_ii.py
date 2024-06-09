d6d167f6a9da8431b956c2e5749aba57a0047c02
taoge64 <a97196487@gmail.com>
2024-06-08 23:48:21 -0700

    Update 210_course-schedule_ii.py

--- Python/210_course-schedule_ii.py
+++ Python/210_course-schedule_ii.py
@@ -1,7 +1,33 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
 class Solution:
+    def findOrderSolution2(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # bfs
+        # i just want to trace all node back
+        preq = defaultdict(set)
+        graph = defaultdict(set)
+        for c, p in prerequisites:
+            preq[c].add(p)
+            graph[p].add(c)
+        queue = deque()
+        res = []
+        for i in range(numCourses):
+            if i not in preq:
+                queue.append(i)
+        taken = []
+        while queue:
+            c = queue.popleft()
+            taken.append(c)
+            if len(taken) == numCourses:
+                return taken
+            for cor in graph[c]:
+                preq[cor].remove(c)
+                if not preq[cor]:
+                    queue.append(cor)
+        # if you have any prerequisite not able to add, you have a cycle
+
+            return []
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         mapping_list = defaultdict(list)
         for c, p in prerequisites:
